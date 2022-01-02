dc4f033d024c5c2ac8d7677039418ce530744a79
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-01-02 09:00:14 +0100

    saving on github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import pic1 from '../images/back-footer.png'
 
 export const Footer = () => {
     return (
         <footer id='footer'>
-            
+            <img className='back-F' src={pic1} alt='back footer'/>
+            <div className='back-I'></div>
+            <div className='container'>
+                <div className='content'>
+                    hello
+                </div>
+            </div>
         </footer>
     )
 }
