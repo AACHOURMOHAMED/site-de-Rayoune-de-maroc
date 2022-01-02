bb23ac9f7ba373a5a53a3c60cb0623275f1a2dee
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-01-02 16:43:45 +0100

    saving on github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import pic1 from '../images/back-footer.png'
+import logo from '../images/logo2.png'
+import { Button } from './Button'
+import {FaUserAlt} from'react-icons/fa'
 
 export const Footer = () => {
     return (
@@ -8,7 +11,20 @@ export const Footer = () => {
             <div className='back-I'></div>
             <div className='container'>
                 <div className='content'>
+                    <img className='logo2' src={logo} />
+                    <center><p className='parg-footer'>
+                                Fondation Mohammed VI de promotion
+                                des œuvres sociales des fonctionnaires
+                                et agents du Ministère des Affaires 
+                                Étrangères et de la Coopération.
+                    </p>
+                        <Button buttonStyle={'btn--circle'}><FaUserAlt className='userbtn'/> </Button>
+                        <Button buttonStyle={'btn--circle'}><FaUserAlt className='userbtn'/> </Button>
+                        <Button buttonStyle={'btn--circle'}><FaUserAlt className='userbtn'/> </Button>
+                        <Button buttonStyle={'btn--circle'}><FaUserAlt className='userbtn'/> </Button>
+
                     
+                    </center>
                 </div>
             </div>
         </footer>
